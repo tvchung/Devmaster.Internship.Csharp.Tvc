9c2c5b74d684d3eec778bca4be4280c0319f7906
tvchung <hna.tvchung@gmmail.com>
2026-08-11 20:03:32 +0700

    Ex01.OrderCalculator

---
+++ Ex01.OrderCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex01.OrderCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
